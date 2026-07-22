8656f96bdc5fe4e5aa0d34ad5ca2c69fe9b4abb8
Aditya Rawat <adityarawatnew2487@gmail.com>
2026-07-22 13:01:05 +0530

    Initiated to new way to operate buttong and flow

--- src/App.tsx
+++ src/App.tsx
@@ -8,69 +8,69 @@ import HideStamps from "./components/HideTimeStamps";
 import TimeTravel from "./components/TimeTravel";
 import EnabledRightClick from "./components/EnabledRightClick";
 import ProvideToken from "./components/ProivideToken";
-import SlideImageCopier from "./components/SlieImageCopierV2";
+// import SlideImageCopier from "./components/SlieImageCopierV2";
 // import MultiplicationTables from "./components/MulitipleTable";
 
 const MainPlayGround = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 gap-2">
       <DisableChat />
+      <OpenRecentLecture />
+
+      <DarkThemScriptForPw />
 
       <DownLoadAttachement />
 
+      <HideStamps />
+
       <OpenBatch
-        name="Lakshya Batch"
+        name="Lakshya 2027"
         id="6779345c20fa0756e4a7fd08"
+      />
+      <OpenBatch
+        name="Lakshya 2026"
+        id="65dc6fbabb55350018d555b7"
       />
 
       <OpenBatch
-        name="Arjuna Batch"
+        name="Arjuna 2026"
         id="676e4dee1ec923bc192f38c9"
       />
 
       <OpenBatch
-        name="JEE One Revision"
+        name="JEE One Shot Revision"
         id="64e4c8338149b2001892a969"
       />
+      <WebsiteOpener
+        name="Lakshya 2026 Test"
+        url="https://www.pw.live/study-v2/batches/65dc6fbabb55350018d555b7/batch-overview?isNewPpjFlow=true&pageName=ALL_TESTS#Tests_6"
+      />
 
-      <OpenRecentLecture />
-
-      <DarkThemScriptForPw />
 
       <WebsiteOpener
         name="Exam Goal"
         url="https://room.examgoal.com/"
       />
 
-      <WebsiteOpener
-        name="Manjil Physical Chemistery"
-        url="https://www.pw.live/watch/?categoryId=68d4e946703add795fe1d8e3&widgetId=6899cf046c4844a03edc43f5&vType=OTT&type=penpencilvdo&childId=69535f62c17f7ff7ac3b41ff&playlistId=69275e714f137fb50d33a5e2"
-      />
-
-      <WebsiteOpener
-        name="Manjil Physical Chemistery"
-        url="https://www.pw.live/watch/?categoryId=68d4e946703add795fe1d8e3&widgetId=6899cf046c4844a03edc43f5&vType=OTT&type=penpencilvdo&childId=69535f62c17f7ff7ac3b41ff&playlistId=69275e714f137fb50d33a5e2"
-      />
 
       <WebsiteOpener
         name="Manjil 2025"
         url="https://www.pw.live/study-v2/batches/671f5ea96059088394262c30/batch-overview"
       />
 
+
       <WebsiteOpener
-        name="PW Mentorshipt"
+        name="PW Mentorship"
         url="https://www.pw.live/study-v2/mentorship/chat/6779345c20fa0756e4a7fd08"
       />
-
-      <HideStamps />
 
       {/* <MultiplicationTables/> */}
 
       <TimeTravel />
-      <ProvideToken/>
+      <ProvideToken />
       <EnabledRightClick />
       {/* <SlideImageCopier /> */}
-      <SlideImageCopier/>
+      {/* <SlideImageCopier /> */}
     </div>
   );
 };
